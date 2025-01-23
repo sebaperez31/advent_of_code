55f2b94fece63c22f9c2642c12fb2a1af683c670
sebaperez31 <40802687+sebaperez31@users.noreply.github.com>
2025-01-23 17:19:45 -0300

    day 11 completed!

--- 2024/day_11/count_stones.py
+++ 2024/day_11/count_stones.py
@@ -1,6 +1,5 @@
-#initial_stones = [6563348, 67, 395, 0, 6, 4425, 89567, 739318]
-initial_stones = [125, 17]
-blinking_times = 25
+initial_stones = [6563348, 67, 395, 0, 6, 4425, 89567, 739318]
+blinking_times = 75
 
 def transform(stone):
     if stone == 0:
@@ -11,14 +10,21 @@ def transform(stone):
         return [ int(stone_str[:int(len_stone_str/2)]), int(stone_str[int(len_stone_str/2):]) ]
     return [ stone * 2024 ]
 
-def blink(stone, times):
+def blink(stone, times, results_dict):
     if times == 0:
         return 1
 
-    return sum([blink(new_stone, times - 1) for new_stone in transform(stone)])
+    if (stone, times) in results_dict:
+        return results_dict[(stone, times)]
+
+    result = sum([blink(new_stone, times - 1, results_dict) for new_stone in transform(stone)])
+    results_dict[(stone, times)] = result
+    return result
 
 result = 0
+results_dict = dict()
 for stone in initial_stones:
-    result += blink(stone, blinking_times)
+    result += blink(stone, blinking_times, results_dict)
 
+print(len(results_dict))
 print(result)
