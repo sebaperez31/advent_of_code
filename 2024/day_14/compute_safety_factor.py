e590b4f870a70bbe2e94b8238a20d39891043a79
sebaperez31 <40802687+sebaperez31@users.noreply.github.com>
2025-02-12 15:32:13 -0300

    day 14 part 1 completed

---
+++ 2024/day_14/compute_safety_factor.py
@@ -0,0 +1,68 @@
+import re
+from collections import namedtuple
+from functools import reduce
+import operator
+
+Robot = namedtuple("Robot", ["px", "py", "vx", "vy"])
+
+def move(robot, times, rows, columns):
+    # compute new_px
+    new_px = robot.px + (times * robot.vx) % columns
+    if new_px < 0:
+        new_px += columns
+    elif new_px >= columns:
+        new_px -= columns
+
+    # compute new_px
+    new_py = robot.py + (times * robot.vy) % rows 
+    if new_py < 0:
+        new_py += rows
+    elif new_py >= rows:
+        new_py -= rows
+    
+    return Robot(new_px, new_py, robot.vx, robot.vy)
+
+def get_quadrant(robot, rows, columns):
+    x_center = columns // 2
+    y_center = rows // 2
+    if robot.px < x_center and robot.py < y_center:
+        return 0
+    elif robot.px > x_center and robot.py < y_center:
+        return 1
+    elif robot.px < x_center and robot.py > y_center:
+        return 2
+    elif robot.px > x_center and robot.py > y_center:
+        return 3
+    else:
+        return None
+
+times = 100
+rows = 103
+columns = 101
+
+robots = []
+with open("complete_data.txt") as file:
+    for line in file:
+        match = re.search(r'p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)', line)
+        px = int(match.group(1))
+        py = int(match.group(2))
+        vx = int(match.group(3))
+        vy = int(match.group(4))
+        robots.append(Robot(px, py, vx, vy))
+
+updated_robots = map(lambda robot : move(robot, times, rows, columns), robots)
+
+robots_per_quadrant = {
+    0 : 0,
+    1 : 0,
+    2 : 0,
+    3 : 0  
+}
+
+for updated_robot in updated_robots:
+    quadrant = get_quadrant(updated_robot, rows, columns)
+    if quadrant != None:
+        robots_per_quadrant[quadrant] = robots_per_quadrant[quadrant] + 1
+
+safety_factor = reduce(operator.mul, robots_per_quadrant.values())
+print(safety_factor)
